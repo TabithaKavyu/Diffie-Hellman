ded83f164a6ef0ffbe05716bb1450966a6f45a3f
tabit <tabithakavyu4@gmail.com>
2020-10-26 15:04:29 +0300

    message encryption and decryption

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 //NAME: KAVYU THABITHA. REG NO: P15/81759/2017
 
-const crypto = require('crypto')
+const crypto = require('crypto'),
+            assert = require('assert'),
+            algorithm = 'aes-256-cbc',
+            IV_LENGTH = 16,
+             DH_LENGTH = 256;
+             
+const alice = crypto.createDiffieHellman(DH_LENGTH);
+const aliceKey = alice.generateKeys();
 
-const alice = crypto.getDiffieHellman('modp15');
+const bob = crypto.createDiffieHellman(alice.getPrime(), alice.getGenerator());
+const bobKey = bob.generateKeys();
 
-const bob = crypto.getDiffieHellman('modp15');
+const aliceSecret = alice.computeSecret(bobKey);
+const bobSecret = bob.computeSecret(aliceKey); // this should be the same as aliceSecret
 
-alice.generateKeys()
-bob.generateKeys()
 
-const aliceSecret = alice.computeSecret(bob.getPublicKey(), null, 'hex');
 
-const bobSecret = bob.computeSecret(alice.getPublicKey(), null, 'hex');
+const password = aliceSecret;
+const iv = crypto.randomBytes(IV_LENGTH).toString('hex').slice(0, IV_LENGTH);
 
-console.log(aliceSecret === bobSecret);
+function encrypt(text){
+  const cipher = crypto.createCipheriv(algorithm, password, iv)
+  const crypted = `${cipher.update(text,'utf8','hex')}${cipher.final('hex')}`
+  return crypted;
+}
 
-console.log(aliceSecret);
-console.log(bobSecret);
+
+function decrypt(text){
+  const decipher = crypto.createDecipheriv(algorithm, password, iv)
+  const dec = `${decipher.update(text,'hex','utf8')}${decipher.final('utf8')}`
+  return dec;
+}
+
+const message =  encrypt('Hello World');
+
+const decryptedMsg = decrypt(message)
+
+console.log(message, decryptedMsg);
+
+
+
 
 
